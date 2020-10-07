a604f16c6ae5e68d29b5cf7bba7e9f32055df34f
gparkkii <jyp933@gmail.com>
2020-10-07 14:40:12 +0900

    Revert "백준 1546 1차원 배열 문제"
    This reverts commit f5b7abd05874e1019bce83d14f65b2416850a69d.

--- BaekJoon_Practice_Bronze/8958.py
+++
@@ -1,9 +0,0 @@
-test_case = int(input())
-test_result = [input() for i in range(test_case)]
-ox_dict = {'O':1, 'X':0}
-ox_count = 0
-
-for j in range(test_result[i]):
-    test = test_result[j]
-    ox_count = ox_dict[test[j]]
-print(ox_count)
